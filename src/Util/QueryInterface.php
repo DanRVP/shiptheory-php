d69c947d9f58cb3c51070a6f7f636468bcf70895
Dan Rogers <77623109+DanRVP@users.noreply.github.com>
2022-11-05 13:27:03 +0000

    Fix further bugs in QueryInterface

--- src/Util/QueryInterface.php
+++ src/Util/QueryInterface.php
@@ -4,11 +4,13 @@ namespace Shiptheory\Util;
 
 interface QueryInterface
 {
+    public function __construct($fields = []);
+
     public function toQueryParams($strict = true);
 
     public function addFields($fields);
 
     public function getFields();
 
-    public function setFields($fields = []);
+    public function setFields($fields);
 }
